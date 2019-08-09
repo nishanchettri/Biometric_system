a19678c4d001327187c6c1d1937f6e323a4d808a
Nishan Chettri <35814272+nishanchettri@users.noreply.github.com>
2019-08-09 12:45:08 +0530

    Add files via upload

---
+++ biometric_security.ino
@@ -0,0 +1,170 @@
+
+/**************************fingersensor*********************/
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+int getFingerprintIDez();
+#define ledPin 13 //Define pin number to which servo motor is connected
+#define durationTime 3000 //Define the time it remains in the open position of the door lock (miliseconds)
+SoftwareSerial mySerial(D1,D2);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+/*****************end*****************/
+/***************rfid******************/
+#define SS_PIN 2  //D4
+#define RST_PIN 0 //D3
+/*
+ SDA(SS)---------> D4
+ SCK---------> D5
+ MOSI--------> D7
+ MISO--------> D6
+ RST---------> D3
+ */
+#include <SPI.h>
+#include <MFRC522.h>
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+int statuss = 0;
+int out = 0;
+/*******************end*****************/
+void setup()  
+{
+  mySerial.begin(115200);
+  Serial.begin(115200);
+  /*************rfid*****************/
+  while (!Serial);    
+  SPI.begin();      // Init SPI bus
+   mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();
+  // set the data rate for the sensor serial port
+  /*************end***************/
+  
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+   if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  
+  //Show UID on serial monitor
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println();
+  if (content.substring(1) == "F5 40 84 43") //change UID of the card that you want to give access
+  {
+    
+    statuss = 1;
+  }
+  
+  else   {
+    Serial.println(" Access Denied ");
+    delay(3000);
+  }
+ getFingerprintIDez();
+//  delay(50);            
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+}
